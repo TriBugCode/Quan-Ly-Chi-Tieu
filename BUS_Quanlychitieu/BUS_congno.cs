48700b44dae7a34b29ce4447cd13cf3eb215687c
TriDepTrai <168658357+TriBugCode@users.noreply.github.com>
2025-03-27 13:13:24 +0700

    Update BUS_congno.cs

--- BUS_Quanlychitieu/BUS_congno.cs
+++ BUS_Quanlychitieu/BUS_congno.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,32 @@ namespace BUS_Quanlychitieu
 {
     internal class congno
     {
+       public class DAL_congno 
+        {
+            DAL_congno dalcongno = new DAL_congno();
+            public DataTable getKhach()
+            {
+                return dalcongno.getKhach();
+            }
+            public bool Insertcongno(DTO_congno khach)
+            {
+                return dalcongno.Insertcongno(khach);
+            }
+            public bool Updatecongno(DTO_congno khach)
+            {
+                return dalcongno.Updatecongno(khach);
+            }
+            public bool Deletecongno(int MaCongNo)
+            {
+                return dalcongno.Deletecongno(MaCongNo);
+            }
+            public DataTable Searchcongno(int MaCongNo)
+            {
+                return dalcongno.Searchcongno(MaCongNo);
+            }
+        }
+        
+        public class DTO_congno 
+        { }
     }
 }
